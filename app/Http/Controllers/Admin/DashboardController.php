eff4ae832530be1e56180c2696306ee8dc683671
abdelilah abdi <abdelilahabdi6633@gmail.com>
2026-04-30 21:21:44 +0100

    edit sur dashboard controller

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -76,7 +76,7 @@ class DashboardController extends Controller
             9 => 'Septembre',
             10 => 'Octobre',
             11 => 'Novembre',
-            12 => 'Decembre',
+            12 => 'Decembre',  //decembre
         ];
 
         $announcements = Announcement::query()
