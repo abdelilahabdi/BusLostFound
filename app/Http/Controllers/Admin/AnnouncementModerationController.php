7ab93299b71ddee8a315a00505d05032bf1a350f
abdelilah abdi <abdelilahabdi6633@gmail.com>
2026-04-30 17:17:48 +0100

    update sur dashboard

--- app/Http/Controllers/Admin/AnnouncementModerationController.php
+++ app/Http/Controllers/Admin/AnnouncementModerationController.php
@@ -51,7 +51,7 @@ class AnnouncementModerationController extends Controller
      */
     public function destroy(Announcement $announcement): RedirectResponse
     {
-        $announcement->delete(); // delet
+        $announcement->delete(); 
 
         return redirect()
             ->route('admin.announcements.index')
