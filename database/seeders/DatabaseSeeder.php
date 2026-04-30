d231a282f2d7d3338855677fe3917a8cf0017a09
abdelilah abdi <abdelilahabdi6633@gmail.com>
2026-04-30 16:18:52 +0100

    modification sur seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
 
         // User::factory(10)->create();
 
-        User::firstOrCreate(   // cheacker data base
+        User::firstOrCreate(   
             ['email' => 'admin@gmail.com'],
             [
                 'name' => 'Abdelilah',
